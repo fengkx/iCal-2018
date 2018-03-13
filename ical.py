16d48c412a021b568920d016b8eee51606d3a474
fengkx <liangkx8237@gmail.com>
2018-03-13 22:21:09 +0800

    fix bug of 2 times TODO:endtime

--- ical.py
+++ ical.py
@@ -2,17 +2,75 @@
 # coding= utf-8
 from bs4 import BeautifulSoup
 import re
+from icalendar import Calendar, Event
+from datetime import datetime
 
 Week = ('一', '二', '三', '四', '五', '六', '日')
-
+Time=[(datetime(2018, 2, 26, 8, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 9, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 10, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 11, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 14, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 15, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 16, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 17, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 19, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 26, 19, 50, tzinfo=pytz.timezone("Asia/Shanghai"))
+)
+,(
+datetime(2018, 2, 27, 8, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 9, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 10, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 11, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 14, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 15, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 16, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 17, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 19, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 27, 19, 50, tzinfo=pytz.timezone("Asia/Shanghai"))
+),(
+datetime(2018, 2, 28, 8, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 9, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 10, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 11, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 14, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 15, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 16, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 17, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 19, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 2, 28, 19, 50, tzinfo=pytz.timezone("Asia/Shanghai"))
+),(
+datetime(2018, 3, 1, 8, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 9, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 10, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 11, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 14, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 15, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 16, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 17, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 19, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 1, 19, 50, tzinfo=pytz.timezone("Asia/Shanghai"))
+),(
+datetime(2018, 3, 2 8, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 9, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 10, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 11, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 14, 30, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 15, 20, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 16, 10, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 17, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 19, 00, tzinfo=pytz.timezone("Asia/Shanghai")),
+datetime(2018, 3, 2 19, 50, tzinfo=pytz.timezone("Asia/Shanghai"))
+)]
 
 def main():
     code = getHTMLText()
     infoDictList = []
     parser(code, infoDictList)
     # print(infoDictList)
-    for item in infoDictList:
-        print(item)
+    # for item in infoDictList:
+    #     print(item)
+    make_cal(infoDictList)
     print("end")
 
 
@@ -36,32 +94,50 @@ def parser(doc, infoDictList):
                 if len(cell) > 1:
                     table.append(cell)
     # print(table)
-
+    lessonList=[]
     for lesson in table:
-        infoDict = {}
-        infoDict.update({'课程名称': lesson[0]})
-        for i in range(len(lesson)):
-            infoDict.update({'课程类型': lesson[1]})
-            infoDict.update({'星期': Week.index(lesson[2][1])+1})
-            rex = re.compile(r'第.*节')
-            count = (rex.findall(lesson[2])[0])
-            # print(count[3:5])
-            length = len(count)
-            if 3 < length < 6:
-                infoDict.update({'节数': (int(count[1]), int(count[3]))})
-            elif length <= 3:
-                infoDict.update({'节数': (int(count[1]),)})
-            else:
-                infoDict.update({'节数': (int(count[1]), int(count[3:5]))})
-            # print(lesson[2])
-            infoDict.update({'周次': (abs(eval(lesson[2][-6:-2]))+1)})
-            infoDict.update({'授课教师': lesson[3]})
-            try:
-                infoDict.update({'上课地点': lesson[4]})
-            except:
-                continue
-            infoDictList.append(infoDict)
-            # print(infoDictList)
+        # print(lesson)
+        lessonList.append(lesson)
+        # infoDict.update({'课程名称': lesson[0]})
+    # print(lessonList)
+    for lesson in lessonList:
+        # print(lesson)
+    # for i in range(len(lesson)):
+        infoDict={}
+        infoDict.update({'课程名称':lesson[0]})
+        # print(infoDict)
+        infoDict.update({'课程类型': lesson[1]})
+        infoDict.update({'星期': Week.index(lesson[2][1])+1})
+        rex = re.compile(r'第.*节')
+        count = (rex.findall(lesson[2])[0])
+        length = len(count)
+        if 3 < length < 6:
+            infoDict.update({'节数': (int(count[1]), int(count[3]))})
+        elif length <= 3:
+            infoDict.update({'节数': (int(count[1]),)})
+        else:
+            infoDict.update({'节数': (int(count[1]), int(count[3:5]))})
+        # print(lesson[2])
+        infoDict.update({'周次': (abs(eval(lesson[2][-6:-2]))+1)})
+        infoDict.update({'授课教师': lesson[3]})
+        try:
+            infoDict.update({'上课地点': lesson[4]})
+        except:
+            continue
+        infoDictList.append(infoDict)
+        print(infoDict)
+def make_cal(infoDictList):
+    cal=Calendar()
+    event1=Event()
+    i=0
+    for j in infoDictList[i][节数]:    
+        event1.add('summary',infoDictList[i]['课程名称'])
+        event1.add('location',infoDictList[i]['上课地点'])
+        event1.add('description',infoDictList[i]['课程类型']+'\n'+infoDictList[i]['授课教师'])
+        event1.add('dtstart',Time[infoDictList['星期'-1]][j-1])
+        event1.add
+
+
 
 
 if __name__ == '__main__':
